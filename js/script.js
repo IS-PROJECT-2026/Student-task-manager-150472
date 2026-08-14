a71c1c4af882317d4e299aaf95b9d76bcec10807
Metabel <metabel.kariri@strathnore.edu>
2026-08-14 13:05:41 +0300

    Implement add task functionality

--- js/script.js
+++ js/script.js
@@ -1,1 +1,19 @@
-console.log("Student Task Manager loaded successfully.");
+const taskInput = document.querySelector("input");
+const addButton = document.querySelector("button");
+const taskList = document.querySelector("ul");
+
+addButton.addEventListener("click", function () {
+    const taskText = taskInput.value.trim();
+
+    if (taskText === "") {
+        alert("Please enter a task.");
+        return;
+    }
+
+    const task = document.createElement("li");
+    task.textContent = taskText;
+
+    taskList.appendChild(task);
+
+    taskInput.value = "";
+});
